9b8e53ff42ed327576ef70ac39053f8e5af3aea9
Grazi <grazi.social84@gmail.com>
2021-09-28 10:41:39 -0300

    login de usuarios

---
+++ src/controllers/login.js
@@ -0,0 +1,42 @@
+const conect = require('../conection_db');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userLogin = async (req, res) => {
+    const {email, senha} = req.body;
+
+    if(!email || !senha){
+        return res.status(404).json('Email ou senha são obrigatórios');
+    }
+
+    try{
+        const qyVerifyEmail = 'select * from usuarios where email = $1';
+        const {rows, rowCount} = await conect.query(qyVerifyEmail, [email]);
+
+        if(rowCount === 0){
+            return res.status(404).json('Esse email é invalido');
+        }
+
+        const usuario = rows[0];
+        const verifiedPassword = await bcrypt.compare(senha, usuario.senha);
+
+        if(!verifiedPassword){
+            return res.status(400).json('Email ou senha incorretos');
+        }
+
+        const token = jwt.sign({id: usuario.id}, 'passwordToken', {expiresIn: '1d'});
+
+        const {senha: senhaUsuario, ...dataUser} = usuario;
+
+        return res.status(200).json({
+            dataUser,
+            token
+        });
+    }catch(error){
+        return res.status(400).json(error.message);
+    }
+}
+
+module.exports = {
+    userLogin
+}
--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const users = require('./controllers/users');
+const userLogin = require ('./controllers/login');
 
 const router = express();
 
 //user register
 router.post('/users', users.userRegister);
+router.post('/login', userLogin.userLogin);
 
 module.exports = router;
